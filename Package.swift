28b27b59c05c0bea038ddc2488d464b381c71e06
IInfo <info@i1nfo.com>
2022-07-20 23:23:03 +0800

    Init commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YXY",
+    products: [
+        .library(
+            name: "YXY",
+            targets: ["YXY"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+		.binaryTarget(
+			name: "YXY",
+			url: "https://github.com/I-Info/yxy/releases/download/v0.1.5/xcframework-universal-v0.1.5.zip",
+			checksum: "03b92a7b6e6a68885936263e0d6204c852cd6c5402500d5a46b3b3499867d942"),
+    ]
+)
