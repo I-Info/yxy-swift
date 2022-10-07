367cc0ca00ba6cea23a68b5e8a016e2b3e98b779
I-Info <49438478+I-Info@users.noreply.github.com>
2022-10-07 06:25:14 +0000

    deps: Update to v0.2.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "yxy",
-            url: "https://github.com/I-Info/yxy/releases/download/v0.2.1+0-1-0.0-1-0/xcframework-universal.zip"
+            url: "https://github.com/I-Info/yxy/releases/download/v0.2.1+0-1-0.0-1-0/xcframework-universal.zip",
             checksum: "e72a90a33f8c0276adc633941f7ce9677641b0e783c26c026086edc74ed41561"
     ]
 )
